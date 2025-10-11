a9f15f21e45c19f101c6dcea3a9bd16bf2a2e468
Mohammedi Abdenasser <abdenasser.mohammedi@outlook.com>
2025-10-11 04:27:09 +0100

    added redirect to google form for testers

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/testers",
+        destination: "https://forms.gle/QR8KQiXuo6oT77KG7",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
